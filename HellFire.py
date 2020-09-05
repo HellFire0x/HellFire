24e8bb55aa21ef54e13b6d19ebf74ed8ef86ebcb
HellFire <vaxure1337@gmail.com>
2020-09-05 20:04:23 -0230

    Initial commit

---
+++ HellFire.py
@@ -0,0 +1,34 @@
+def Logo():
+    Name = """
+          ___ ___         .__  .__  ___________.__                
+         /   |   \   ____ |  | |  | \_   _____/|__|______   ____  
+        /    ~    \_/ __ \|  | |  |  |    __)  |  \_  __ \_/ __ \ 
+        \    Y    /\  ___/|  |_|  |__|     \   |  ||  | \/\  ___/ 
+         \___|_  /  \___  >____/____/\___  /   |__||__|    \___  >
+               \/       \/               \/                    \/ 
+       
+                          9 H I T S   E X P L O I T
+        ___________________________________________________________
+                                Version: 1.0"""
+    print("\033[6m" + Name + "\033[0m")
+    print("                              Made By:\033[91m HellFire \033[0m")
+    print("                            Helper:\033[34m Dean Richards \033[0m")
+
+def main():
+    Logo()
+    Command = [
+    "        1) 9Hits Data",
+    "        2) API Finder",
+    "        3) 9Hits Links",
+    ""]
+
+    for x in Command:
+        print(x)
+
+    Cmd = input("        [+]> ")
+    if Cmd == "1":
+        #Command 1
+        print("")
+
+
+main()
